e9914bcf7adcd65734248569eda4777a2ed0f7b9
MW-7892 <sk.14055450@gmail.com>
2024-11-11 01:25:37 +0900

    feat: connect to apollo gql

---
+++ app/components/graphql/ApolloWrapper.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import { ApolloLink, from, HttpLink } from "@apollo/client";
+import { ApolloClient, ApolloNextAppProvider, InMemoryCache } from "@apollo/experimental-nextjs-app-support";
+import { setContext } from "apollo-link-context";
+
+export const makeClient = () => {
+  const httpLink = new HttpLink({
+    uri: process.env.NEXT_PUBLIC_API_SERVER,
+  })
+
+  const authMiddleware = setContext(async (_, { headers }) => {
+    const token = localStorage.getItem('minigrader-token')
+
+    return {
+      headers: {
+        ...headers,
+        ...(token ? {Authorization: `Bearer ${token}`} : {}),
+      },
+    }
+  })
+
+  return new ApolloClient({
+    link: from([(authMiddleware as unknown) as ApolloLink, httpLink]),
+    cache: new InMemoryCache(),
+  })
+}
+
+export default function ApolloWrapper({ children }: React.PropsWithChildren) {
+  return (
+    <ApolloNextAppProvider makeClient={makeClient}>
+      { children }
+    </ApolloNextAppProvider>
+  )
+}
