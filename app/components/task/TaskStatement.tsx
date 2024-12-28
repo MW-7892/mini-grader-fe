ba0828922790fdd6eb46e87b34f20558c062ae71
MW-7892 <sk.14055450@gmail.com>
2024-12-28 23:24:04 +0900

    feat: code highlight in statement

--- app/components/task/TaskStatement.tsx
+++ app/components/task/TaskStatement.tsx
@@ -1,13 +1,14 @@
-import Markdown from "react-markdown";
-import rehypeKatex from "rehype-katex";
-import remarkMath from "remark-math";
+import Markdown from "react-markdown"
+import rehypeKatex from "rehype-katex"
+import remarkMath from "remark-math"
 import "katex/dist/katex.min.css"
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
 
 export default function TaskStatement({
   full_name,
   time_limit,
   memory_limit,
-  statement
+  statement,
 }: {
   full_name: string
   time_limit: number
@@ -15,24 +16,52 @@ export default function TaskStatement({
   statement?: string | null
 }) {
   const ONE_GIGABYTE = 1024
-  const formatMemoryLimit = (memory_limit: number) => {
-    if (memory_limit > ONE_GIGABYTE * 3) return `${(memory_limit / ONE_GIGABYTE).toPrecision(3)} GB` 
-    return `${memory_limit} MB` 
+  const formatMemoryLimit = (memory: number) => {
+    if (memory > ONE_GIGABYTE * 3) return `${(memory / ONE_GIGABYTE).toPrecision(3)} GB`
+    return `${memory} MB`
   }
+
   return (
     <div className="markdown relative">
-      <h1>{ full_name }</h1>
+      <h1>{full_name}</h1>
       <div
         className="lg:absolute lg:top-0 lg:right-0 bg-gray-100 rounded-full px-3 py-1 m-2
           text-sm ml-0 mb-3"
       >
-        { time_limit / 1000 } { time_limit === 1000 ? "second" : "seconds" } { " / " }
-        { formatMemoryLimit(memory_limit) }
+        {time_limit / 1000} {time_limit === 1000 ? "second" : "seconds"} {" / "}
+        {formatMemoryLimit(memory_limit)}
       </div>
       <Markdown
-        remarkPlugins={[ remarkMath ]}
-        rehypePlugins={[ rehypeKatex ]}
-      >{ statement }</Markdown>
+        remarkPlugins={[remarkMath]}
+        rehypePlugins={[rehypeKatex]}
+        components={{
+          code({ inline, className, children, ...props }: any) {
+            const match = /language-(\w+)/.exec(className || "")
+
+            return !inline && match ? (
+              <SyntaxHighlighter
+                language={match[1]}
+                customStyle={{
+                  borderRadius: "0.375rem",
+                  fontSize: "0.875rem",
+                  lineHeight: "1.25rem",
+                  marginBottom: "1rem",
+                  backgroundColor: "#F3F4F6",
+                }}
+                {...props}
+              >
+                {String(children).replace(/\n$/, "")}
+              </SyntaxHighlighter>
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            )
+          },
+        }}
+      >
+        {statement}
+      </Markdown>
     </div>
   )
 }
--- app/components/task/TaskStatementForm.tsx
+++ app/components/task/TaskStatementForm.tsx
@@ -2,6 +2,7 @@ import { Textarea } from "@headlessui/react"
 import { Roboto_Mono } from "next/font/google"
 import { ChangeEvent } from "react"
 import TaskStatement from "./TaskStatement"
+import "highlight.js/styles/default.css"
 
 const editorFont = Roboto_Mono({
   subsets: ["latin"],
@@ -27,7 +28,7 @@ export default function TaskStatementForm({
         name="statement"
         value={statement}
         onChange={handleStatementChange}
-        className={`resize-none focus:outline-none border border-gray-200 dark:bg-dark
+        className={`resize-none focus:outline-none border border-gray-200 dark:bg-dark language-markdown
           rounded-lg p-4 ${editorFont.className} h-screen md:h-full mb-6 md:mb-0 dark:text-gray-300`}
       ></Textarea>
       <div className="hidden md:block absolute top-0 left-1/2 h-full border border-dashed border-l-gray-100"></div>
