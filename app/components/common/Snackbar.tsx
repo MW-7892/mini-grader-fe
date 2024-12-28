3319e0198616d4687efce2e0cacf83639722855a
MW-7892 <sk.14055450@gmail.com>
2024-12-28 22:30:56 +0900

    feat: snackbar from above instead of bottom right

--- app/components/common/Snackbar.tsx
+++ app/components/common/Snackbar.tsx
@@ -3,24 +3,23 @@
 import { MdInfoOutline } from "react-icons/md";
 import { useSnackbar } from "./SnackbarProvider";
 import { useEffect } from "react";
+import { setTimeout } from "timers";
 
 export default function Snackbar() {
   const { message, setMessage } = useSnackbar()
 
   useEffect(() => {
+    if (!message) return
+
     const snackbar = document.getElementById("snackbar")
-    snackbar?.classList.toggle("hidden")
-    snackbar?.classList.toggle("opacity-0")
-    snackbar?.classList.toggle("flex")
-    new Promise((resolve, _) => setTimeout(() => {
-      snackbar?.classList.toggle("opacity-0")
-      // Gotta wait for the animation
-      setTimeout(() => resolve(true), 1000)
-    }, 5000))
-      .then(() => {
-        snackbar?.classList.toggle("flex")
-        snackbar?.classList.toggle("hidden")
-      })
+    new Promise((resolve, _) => {
+      snackbar?.classList.toggle("-translate-y-32")
+      setTimeout(() => resolve(true), 4000)
+    })
+      .then(() => new Promise((resolve, _) => {
+        snackbar?.classList.toggle("-translate-y-32")
+        setTimeout(() => resolve(true), 1000)
+      }))
       .then(() => setMessage(null))
       .catch(error => console.log(error))
   }, [message])
@@ -29,9 +28,10 @@ export default function Snackbar() {
     <div
       id="snackbar"
       key={message}
-      className={`fixed bottom-0 right-0 m-10 w-fit h-fit cursor-default
-      bg-black/80 rounded-md py-3 px-5 gap-x-3 text-white justify-center
-      items-center ease-in-out transition-opacity opacity-0 z-100 hidden`}
+      className={`fixed top-0 left-1/2 translate-x-[-50%] m-10 w-fit h-fit
+        cursor-default -translate-y-32 flex
+        bg-black/80 rounded-md py-3 px-5 gap-x-3 text-white justify-center
+        items-center duration-200 ease-in-out z-100`}
     >
       <MdInfoOutline />
       { message }
